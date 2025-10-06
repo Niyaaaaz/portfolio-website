3a84a99e28f137757f6eb23a8bfc33a32f1779d3
Niyaaaaz <niyazaalira10@gmail.com>
2025-10-06 14:56:37 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+###
+git hub link :
+deployed link: 
---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <header>
+        <h1 className='Name'>Name : Niyaz</h1>
+        <title>Frontend developer</title>
+      </header>
+      <div>
+        <p>Iam Niyaz from AIFT . iam from kodagu karnataka. I will play football and my ambission is to be an web developer</p>
+      </div>
+      <section>
+        <h2 className='pro'>Project1</h2>
+        <h2>Project2</h2>
+      </section>
+      <footer>
+        <p>Contact me : 8050422380</p>
+      </footer>
+    </>
+  )
+}
+
+export default App
